c1a80f20a2c90c6ae256421bc12f89b793419f8d
jmartingimenez <jmartingimenez@hotmail.com>
2018-04-20 12:01:11 -0300

    Testeando el .gitignore V2

--- TPFinalProgWebIII/TPFinalProgWebIII/Models/BorrarLuego.cs
+++ TPFinalProgWebIII/TPFinalProgWebIII/Models/BorrarLuego.cs
@@ -5,5 +5,9 @@ using System.Web;
 
 namespace TPFinalProgWebIII.Models {
     public class BorrarLuego {
+        /*Si la carpeta Models esta vacia, no aparece en el repo.
+         Puse esto nomas para que quede creada hasta que empecemos a 
+         agregar cosas...*/
+        private string puteadaFavorita = "";
     }
 }
