ec7eb8df217ebd61bc132918e64a938d407f60d1
Marcos Betancor <marcosbetancorl@gmail.com>
2018-05-01 17:57:15 -0300

    Creada interfaz IGeneralService con metodos comunes a todos

---
+++ TPFinalProgWebIII/TPFinalProgWebIII/Models/Interface/GeneralService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPFinalProgWebIII.Models.Interface
+{
+    interface IGeneralService<T>
+    {
+        T Crear(T a);
+
+        T Modificar(T a);
+
+        void Eliminar(T a);
+
+        List<T> Listar();
+
+    }
+}
