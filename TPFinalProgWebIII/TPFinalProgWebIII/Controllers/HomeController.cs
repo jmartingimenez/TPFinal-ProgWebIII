b7f32ed548b6bb52fe3c9082aa7fcebc97f078e4
jmartingimenez <jmartingimenez@hotmail.com>
2018-05-13 16:40:04 -0300

    Se pueden registrar los usuarios

--- TPFinalProgWebIII/TPFinalProgWebIII/Controllers/HomeController.cs
+++ TPFinalProgWebIII/TPFinalProgWebIII/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using BotDetect.Web.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -80,13 +82,93 @@ namespace TPFinalProgWebIII.Controllers
         public ActionResult Registracion(Registro registro)
         {
             if (!ModelState.IsValid) return View("Registracion", registro);
-            ViewData["MensajeOK"] = "Todo OK. Ahora habría que ir a la BDD.";
+
+            //Lo que esta aca, pasarlo luego a servicios
+            ViewData["MensajeOK"] = this.GestionRegistro(registro);
+
             return View("Registracion");
         }
 
         public ActionResult Logout()
         {
             return View();
+        }
+
+        /*===========================================================
+         * ESTO SE TIENE QUE SACAR DE ACA LUEGO, PASARLO A SERVICIOS
+         ===========================================================*/
+
+        private Usuario CrearUsuario(Usuario usuario, Registro registro)
+        {
+            usuario.Nombre = registro.Nombre;
+            usuario.Apellido = registro.Apellido;
+            usuario.Email = registro.Email;
+            usuario.Contrasenia = registro.Contrasenia;
+
+            /*Hay que ver luego como generarlo y como el usuario 
+             se dara de alta (ej. mandar un mail)*/
+            usuario.CodigoActivacion = "RANDOM123";
+
+            /*Esto debería hacerlo solo la BDD, pero no lo hace. 
+             El campo no puede ser nulo*/
+            usuario.FechaRegistracion = DateTime.Now;
+
+            return usuario;
+        }
+            
+        /*Cuando se saque esto de aca. Desarmarlo en métodos mas 
+        chicos si es posible. Estoy viendo si el user existe en 
+        la BDD, y si existe si es activo o no el user. Por eso quedo 
+        largo*/
+        private string GestionRegistro(Registro registro)
+        {
+            string mensaje = "El mail se encuentra en uso.";
+            Usuario usuario = db.Usuario.FirstOrDefault(x => x.Email == registro.Email);
+
+            if (usuario == null)    
+            {
+                usuario = CrearUsuario(new Usuario(), registro);
+                mensaje = "Nuevo registro agregado a la BDD.";
+                db.Usuario.Add(usuario);
+                db.Entry(usuario).State = System.Data.EntityState.Added;
+            }
+            else                    
+            {
+                if (usuario.Activo == 1)
+                    return mensaje;
+
+                usuario = CrearUsuario(usuario, registro);
+                mensaje = "Se actualizo un usuario viejo.";
+                db.Entry(usuario).State = System.Data.EntityState.Modified;
+            }
+
+            /*El catch lo encontre en SO. Te imprime errores de 
+             insersión, por ejemplo: Un campo que no puede ser 
+             NULL y no se lo estas dando.*/
+            try
+            {
+                db.SaveChanges();
+                return mensaje;
+            }
+            catch (DbEntityValidationException e)
+            {
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    Debug.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                        eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        Debug.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                            ve.PropertyName, ve.ErrorMessage);
+                    }
+                }
+                throw;
+            }
+            catch(Exception e)
+            {
+                Debug.Write("\n\t\tALGUN ERROR, VER QUE PASA\n\n");
+                throw;
+            }
         }
     }
 }
