ea5fbcc3676fe6cb5a4d32223c85c41e7e688881
jmartingimenez <jmartingimenez@hotmail.com>
2018-05-13 18:06:35 -0300

    Probando un controller padre para controlar la sesión

---
+++ TPFinalProgWebIII/TPFinalProgWebIII/Controllers/CustomController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TPFinalProgWebIII.Controllers
+{
+    public class CustomController : Controller
+    {
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (Session["Nombre"].Equals(String.Empty))
+            {
+                //Para obtener a donde se queria ingresar
+                string actionName = this.ControllerContext.RouteData.Values["action"].ToString();
+                string controllerName = this.ControllerContext.RouteData.Values["controller"].ToString();
+
+                Debug.WriteLine("\n\t\t\t=========================="+ 
+                    actionName + ", " + controllerName);
+
+                filterContext.Result = RedirectToAction("Login", "Home");
+            }               
+        }
+    }
+}
