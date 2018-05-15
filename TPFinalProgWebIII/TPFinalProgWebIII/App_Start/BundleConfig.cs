8c65ce0df6e58f1f6233fe2d724d8e4aa08696be
Marcos Betancor <mbetancor.mb@gmail.com>
2018-05-15 14:37:50 -0300

    Agregado Building And Minification

---
+++ TPFinalProgWebIII/TPFinalProgWebIII/App_Start/BundleConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace TPFinalProgWebIII.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                 "~/Content/js/jquery*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                 "~/Content/js/modernizr-2.*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Content/js/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrapcss").Include(
+                      "~/Content/css/bootstrap.css"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
