89f082dc808172e2fa635aafeeed3c22908392a8
jmartingimenez <jmartingimenez@hotmail.com>
2018-06-23 16:33:12 -0300

    Agregadas las clases de apoyo del ejemplo

---
+++ TPFinalProgWebIII/TPFinalProgWebIII/Models/Util/StringUtility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TPFinalProgWebIII.Models.Util {
+    public static class StringUtility {
+        //es un extension method
+        public static string Truncar(this string value, int maxLength)
+        {
+            if (string.IsNullOrEmpty(value)) return value;
+            return value.Length <= maxLength ? value : value.Substring(0, maxLength);
+        }
+    }
+}
